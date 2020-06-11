183310a29ade0d0cdb54755854aab958fd1dbc40
Jose Chavez <joseacb144@gmail.com>
2020-06-11 01:08:46 -0500

    Update app.js app.listen feature

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ app.post("/failure", function(req,res){
 
 // Port was changed from local (3000) to process.evn.PORT for heroku
 // process.evn.PORT || 3000 allows you to run the web app locally and on heroku
-app.listen(process.evn.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000.");
-})
+});
 
 
 // API KEY
