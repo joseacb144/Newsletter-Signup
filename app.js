7f431e8e68e2393250e9a7d542c333c66023d556
Jose Chavez <joseacb144@gmail.com>
2020-06-10 19:27:42 -0500

    Update to app.js to console log the request items (fName, lName, email). Also, update the html file.

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ const request = require("request");
 
 const app = express();
 
+// This is used to incorporate styles.css and images to the html file
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req,res){
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/signup.html");
 })
+
+app.post("/",function(req,res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
+
+})
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
